7a79fd87543f751d3e87e49cc655f99da024ad77
sahilm3 <90495590+sahilm3@users.noreply.github.com>
2022-05-27 17:23:38 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get('API_ID', 0))
-    API_HASH = os.environ.get('API_HASH', None)
-    TOKEN = os.getenv("token","xxxxx")
+    API_ID = int(os.environ.get('API_ID', 8763712))
+    API_HASH = os.environ.get('API_HASH', "835d27216f117e22a5c192b89a4ce457")
+    TOKEN = os.getenv("token","5383986748:AAF6E0nTxExvqSMMdChsnSBA-yUXVxrdZ8U")
     DOMAIN  = os.getenv("domain","https://newdlstar.herokuapp.com")
-    CHANNEL = int(os.getenv("channel","-1001249461809") )
+    CHANNEL = int(os.getenv("channel","-1001631582129") )
